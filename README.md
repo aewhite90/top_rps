7b34190f4cdecb9724282e2e7da9543243406bd4
aewhite90 <white.andrew.e@icloud.com>
2021-03-16 03:11:25 -0400

    File clean-up

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 Rock Paper Scissors -- theOdinProject
+2021.03.14 -- RPS w/ GUI
+
+Rock, Paper, Scissors built for the Odin Project.  Initially built to work in
+the browser console, but reworked to include a fully interactive front-end
+with JavaScript updating the display throughout a game to score 5 wins before
+the computer does.
--- script.js
+++ script.js
@@ -1,15 +1,39 @@
+let reset = () => {
+  location.reload();
+}
+
+let end = () => {
+  const buttons = Array.from(document.querySelectorAll('.btn-outline-light'));
+  buttons.forEach(key => key.classList.add('disabled'));
+}
+
+let removeTransition = (e) => {
+  const buttons = Array.from(document.querySelectorAll('.btn'));
+  const lastPlays = Array.from(document.querySelectorAll('.pulse'));
+  buttons.forEach(key => key.addEventListener('click', key.classList.remove('played')));
+  buttons.forEach(key => key.addEventListener('click', key.classList.remove('pulse')));
+}
+
+
 let computerPlay = () => {
     let compMove = Math.floor(Math.random()*3);
     switch(compMove) {
-      case 0: return 'rock';
-      case 1: return 'paper';
-      case 2: return 'scissors';
+      case 0:
+        document.getElementById('compRock').classList.add('played');
+        document.getElementById('compRock').classList.add('pulse');
+        return 'rock';
+      case 1:
+        document.getElementById('compPaper').classList.add('played');
+        document.getElementById('compPaper').classList.add('pulse');
+        return 'paper';
+      case 2:
+        document.getElementById('compScissors').classList.add('played');
+        document.getElementById('compScissors').classList.add('pulse');
+        return 'scissors';
     }
 }
 
 let playRound = (playerSelection, computerSelection) => {
-  playerSelection = playerSelection.toLowerCase();
-  computerSelection = computerSelection.toLowerCase();
   if (playerSelection == computerSelection) {
     return "Draw!"
   } else if (playerSelection == 'rock') {
@@ -24,13 +48,52 @@ let playRound = (playerSelection, computerSelection) => {
   }
 }
 
-const computerSelection = computerPlay();
+let playerWins = 0;
+let computerWins = 0;
 
-let game = () => {
-  let rounds = 5;
-  while (rounds > 0) {
-    let playerSelection = prompt('Enter Rock, Paper, or Scissors:');
-    alert(playRound(playerSelection, computerSelection));
-    rounds--;
+let game = (e) => {
+  const gameResultsDiv = document.getElementById('gameResults');
+  const score = document.getElementById('score');
+  const winnerAnnouncement = document.getElementById('winner');
+  const resetButton = document.getElementById('resetButton');
+  gameResultsDiv.style.display = 'flex';
+  if (playerWins == 5) {
+    resetButton
+    return;
+  };
+  if (computerWins == 5) {
+    resetButton();
+    return;
+  };
+  playerSelection = e.currentTarget.id;
+  removeTransition();
+  const button = document.getElementById(playerSelection);
+  button.classList.add('played');
+  button.classList.add('pulse');
+  let computerSelection = computerPlay();
+  let currentRound = playRound(playerSelection, computerSelection)
+  if (currentRound == 'You win!') {
+    playerWins++;
+  } else if (currentRound == 'You lose!') {
+    computerWins++;
   }
+  score.textContent = `You: ${playerWins} || Computer: ${computerWins}`;
+  if (playerWins == 5) {
+    winnerAnnouncement.textContent = 'YOU BEAT THE COMPUTER!';
+    resetButton.style.display = 'flex';
+    end();
+    return;
+  };
+  if (computerWins == 5) {
+    winnerAnnouncement.textContent = 'THE COMPUTER BEAT YOU!';
+    resetButton.style.display = 'flex';
+    end();
+    return;
+  };
 }
+
+document.getElementById('rock').addEventListener('click', game);
+document.getElementById('paper').addEventListener('click', game);
+document.getElementById('scissors').addEventListener('click', game);
+
+document.getElementById('reset').addEventListener('click', reset);
